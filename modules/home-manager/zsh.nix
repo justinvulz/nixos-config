ec53f6527827f1b659e7d444025b3f5db290c863
justin <justin.chen@thinktrace.com>
2024-08-14 16:46:48 +0800

    install ohmyzsh change theme

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -1,17 +1,23 @@
 {pkgs, lib, config, ... }: 
 
 {
-    programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-        shellAliases = {
-            ll = "ls -l";
-        };
-        history = {
-            size = 100;
-            path = "${config.xdg.dataHome}/zsh/history";
-        };
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      cls = "clear";
     };
+    history = {
+      size = 100;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "thefuck" ];
+      theme = "robbyrussell";
+    };
+  };
 }
