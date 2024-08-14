c26e02dbeb739e574fdb3ddb2c012067817f5dd5
justin <justin.chen@thinktrace.com>
2024-08-14 17:28:45 +0800

    do

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -20,13 +20,18 @@
     };
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "thefuck" ];
+      plugins = [ "git" ];
     };
     plugins = [
       {
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
       }
     ];
   };
