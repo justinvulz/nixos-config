d8714808d2a174bf09cdffbd9a545fe6e49c60b2
justin <justin.chen@thinktrace.com>
2024-08-14 15:53:08 +0800

    fix bug

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./../modules/home-manager/git.nix
+    ./../../modules/home-manager/git.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
