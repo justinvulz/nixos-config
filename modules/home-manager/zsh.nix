47f50f325470497590ab61de0cb45b1a99b27ca0
justin <justin.chen@thinktrace.com>
2024-08-14 17:03:06 +0800

    fix

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -18,6 +18,12 @@
       enable = true;
       plugins = [ "git" "thefuck" ];
     };
-    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ]
   };
 }
