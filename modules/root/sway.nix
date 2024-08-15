3f28bebd9d8717d2a208980bbe4e8949b391f24e
justin <justin.chen@thinktrace.com>
2024-08-15 13:25:52 +0800

    test

--- modules/root/sway.nix
+++ modules/root/sway.nix
@@ -3,15 +3,12 @@
 {
   
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  programs.xwayland.enable = true;
-  security.pam.services.swaylock = {};
-
+  
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true; # so that gtk works properly
