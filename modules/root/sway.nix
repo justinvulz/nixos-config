6f1fda04b234b1ea59c25f9d2a56597b6a2b2fa0
justin <justin.chen@thinktrace.com>
2024-08-15 10:14:56 +0800

    test

--- modules/root/sway.nix
+++ modules/root/sway.nix
@@ -3,12 +3,13 @@
 {
   
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  programs.xwayland.enable = true;
 
   programs.sway = {
     enable = true;
