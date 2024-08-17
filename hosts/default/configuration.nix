b8b5512de3e923cebfe66b201709b7f4e142f60c
justin <justin.chen@thinktrace.com>
2024-08-17 16:34:14 +0800

    test

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -145,4 +145,16 @@
     dates = "weekly";
     options = "--delete-older-than 30d";
   };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
 }
--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -1,17 +1,7 @@
 { config, pkgs,lib, inputs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-  ];
+  
   
   stylix.enable = true;
   # stylix.polarity = "dark";
