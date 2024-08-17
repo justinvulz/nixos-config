3b063842e705a2ab14ac6c87053ebcdcd1468076
justin <justin.chen@thinktrace.com>
2024-08-17 20:10:25 +0800

    chnage wallpaper

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -5,7 +5,7 @@
   
   stylix.enable = true;
   # stylix.polarity = "dark";
-  stylix.image = ./../../config/wallpaper/01.jpg;
+  stylix.image = ./../../config/wallpaper/02.jpg;
   stylix.fonts = {
     serif = {
       package = pkgs.dejavu_fonts;
