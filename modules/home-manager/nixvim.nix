0fe80be9777909b08189dd106610816e97de5175
justin <justin.chen@thinktrace.com>
2024-08-17 21:46:42 +0800

    nixvim

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -3,12 +3,13 @@
 {
 	programs.nixvim = {
 		enable = true;
-		options = {
+		opts = {
 			number = true;
 			relativenumber = true;
-			
 		};
-		colorschemes.gruvbox.enable = true;
+		# colorschemes.gruvbox.enable = true;
 		plugins.lightline.enable = true;
+		extraPlugins = [ pkgs.vimPlugins.gruvbox ];
+    colorscheme = "gruvbox";
 	};
 }
