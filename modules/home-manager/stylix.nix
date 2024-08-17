b4d4b54b9a684f8fc90cd52d0708632298cc383c
justin <justin.chen@thinktrace.com>
2024-08-17 16:17:33 +0800

    test

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -4,26 +4,26 @@
   stylix.enable = true;
   # stylix.polarity = "dark";
   stylix.image = ./../../config/wallpaper/01.jpg;
-  # stylix.fonts = {
-  #   serif = {
-  #     package = pkgs.dejavu_fonts;
-  #     name = "DejaVu Serif";
-  #   };
+  stylix.fonts = {
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+    };
 
-  #   sansSerif = {
-  #     package = pkgs.dejavu_fonts;
-  #     name = "DejaVu Sans";
-  #   };
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
 
-  #   monospace = {
-  #     package = pkgs.dejavu_fonts;
-  #     name = "DejaVu Sans Mono";
-  #   };
+    monospace = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans Mono";
+    };
 
-  #   emoji = {
-  #     package = pkgs.noto-fonts-emoji;
-  #     name = "Noto Color Emoji";
-  #   };
-  # };
+    emoji = {
+      package = pkgs.noto-fonts-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
 
 }
