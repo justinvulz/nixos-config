9ae4edbf68b1e45f3762e45da9932ab7b9818cb2
justin <justin.chen@thinktrace.com>
2024-08-17 22:00:06 +0800

    feat: enable lightline, bufferline, and treesitter plugins in nixvim.nix

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -8,8 +8,12 @@
 			relativenumber = true;
 		};
 		# colorschemes.gruvbox.enable = true;
-		plugins.lightline.enable = true;
 		extraPlugins = [ pkgs.vimPlugins.gruvbox ];
     colorscheme = "gruvbox";
+		plugins = {
+			lightline.enable = true;
+			bufferline.enable = true;
+			treesitter.enable = true;
+		};
 	};
 }
