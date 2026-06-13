37220717c456ff0eb28c32db43d0c9ad090e6446
justinChen <justin.chen@thinktrace.com>
2026-06-13 14:06:05 +0800

    backup

--- hosts/mars/configuration.nix
+++ hosts/mars/configuration.nix
@@ -1,7 +1,5 @@
 {
-  config,
   pkgs,
-  lib,
   inputs,
   ...
 }:
@@ -10,122 +8,27 @@
   imports = [
     ./hardware-configuration.nix
     ./spmodule/nvidia.nix
-    # ./../../modules/nixos/sway.nix
-    # ./../../modules/nixos/gnome.nix
-    # ./../../modules/nixos/cosmic.nix
-    ./../../modules/nixos/hyprland.nix
-    ./../../modules/nixos/htop.nix
-    # ./../../modules/nixos/avahi.nix
-
-    ./../../modules/nixos/steam.nix
-    ./../../modules/nixos/nbfc.nix
-    ./../../modules/nixos/ssh.nix
-    ./../../modules/nixos/docker.nix
-    ./../../modules/nixos/cache.nix
-  ];
+  ]
+  ++ (with inputs.self.nixosModules; [
+    essential
+    niri
+    htop
+    steam
+    ssh
+    docker
+    fonts
+    ratbagd
+    nbfc
+  ]);
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [
-    "ntfs"
-    "zfs"
-    "apfs"
-  ];
 
-  networking.hostName = "justin-msi"; # Define your hostname.
+  networking.hostName = "mars"; # Define your hostname.
   networking.hostId = "37abf3e1";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    input = {
-      General = {
-        UserspaceHID = true;
-      };
-    };
-  };
-  services.blueman.enable = true;
-  # Set your time zone. time.timeZone = "Asia/Taipei";
-  time.timeZone = "Asia/Taipei";
-
-  # CPU thermald Bootting
-  services.thermald.enable = true;
-  powerManagement.cpuFreqGovernor = "performance";
 
   # fIX MSI FAN ?!!!!
   boot.kernelModules = [ "ec_sys" ];
   boot.kernelParams = [ "ec_sys.write_support=1" ];
-  # boot.extraModprobeConfig = ''
-  #  options ec_sys write_support=1
-  # '';
-  # Select internationalisation properties.
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "zh_TW.UTF-8";
-    LC_IDENTIFICATION = "zh_TW.UTF-8";
-    LC_MEASUREMENT = "zh_TW.UTF-8";
-    LC_MONETARY = "zh_TW.UTF-8";
-    LC_NAME = "zh_TW.UTF-8";
-    LC_NUMERIC = "zh_TW.UTF-8";
-    LC_PAPER = "zh_TW.UTF-8";
-    LC_TELEPHONE = "zh_TW.UTF-8";
-    LC_TIME = "zh_TW.UTF-8";
-  };
-
-  i18n.inputMethod = {
-    type = "fcitx5";
-    enable = true;
-    fcitx5.addons = with pkgs; [
-      rime-data
-      fcitx5-gtk
-      fcitx5-rime
-      fcitx5-nord
-    ];
-    fcitx5.waylandFrontend = true;
-  };
-
-  #
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable polkit for allowing unprivileged users to run administrative tasks.
-  security.polkit.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.justin = {
@@ -135,26 +38,7 @@
       "networkmanager"
       "wheel"
     ];
-    # packages = with pkgs;
-    #   [
-    #     #  thunderbird
-    #   ];
   };
-
-  # uninstall the nixos documentation
-  documentation.nixos.enable = false;
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Install zsh.
-  # programs.zsh.enable = true;
-  # users.defaultUserShell = pkgs.zsh;
-  users.defaultUserShell = pkgs.nushell;
-  # environment.pathsToLink = [ "/share/zsh" ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -175,29 +59,6 @@
     ];
   };
 
-  # vm
-  # virtualisation.virtualbox.host.enable = true;
-  # users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -205,37 +66,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  # auto delete old generations
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-  nix.optimise.automatic = true;
-
-  fonts.packages = with pkgs; [
-    noto-fonts-cjk-sans
-    noto-fonts
-    noto-fonts-color-emoji
-    liberation_ttf
-    # fira-code
-    # fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    meslo-lgs-nf
-    wqy_zenhei
-    wqy_microhei
-    source-han-sans
-    nerd-fonts.fira-code
-    nerd-fonts.fira-mono
-  ];
-  fonts.fontDir.enable = true;
 
 }
